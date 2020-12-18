63df5636a2fce556e84b0d06574e138d01a877fd
Muli Cohen <mulicohen@Mulis-MacBook-Pro.local>
2020-12-18 11:32:39 +0200

    final class solution

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Button from "@material-ui/core/Button";
 import CssBaseline from "@material-ui/core/CssBaseline";
@@ -13,19 +13,8 @@ import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import { useForm } from "react-hook-form";
+import axios from "axios";
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -40,7 +29,10 @@ const useStyles = makeStyles((theme) => ({
   },
   form: {
     width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1)
+    marginTop: theme.spacing(1),
+  },
+  formHide: {
+    display: "none"
   },
   submit: {
     margin: theme.spacing(3, 0, 2)
@@ -52,8 +44,23 @@ export default function SignIn() {
   const { register, handleSubmit, errors } = useForm({
     mode: 'onBlur', //best option
   });
-  
+  const [message, setMessage] = useState("")
+
+  function handleClick(data) {
+    axios.post('https://simple-node-post.herokuapp.com/', {
+      data: data
+    })
+    .then(function (response) {
+      console.log(response);
+      setMessage(response.data)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
   return (
+    <div>
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
@@ -64,10 +71,48 @@ export default function SignIn() {
           Sign in
         </Typography>
         <form
-          className={classes.form}
+          className={!message ? classes.form : classes.formHide}
           noValidate
-          onSubmit={handleSubmit((data) => alert(JSON.stringify(data)))}
+          onSubmit={handleSubmit((data) => handleClick(data))}
         >
+          <TextField
+            variant="outlined"
+            margin="normal"
+            inputRef={register({
+              required: 'You must provide a name.',
+              maxLength: {
+                value: 12,
+                message: 'Your name must be upto 12 characters',
+              },
+            })}
+            required
+            fullWidth
+            name="name"
+            label="Name"
+            autoComplete="current-password"
+          />
+          {errors.name && <div>{errors.name.message}</div>}
+
+
+          <TextField
+              // onChange={handleChange}
+              variant="outlined"
+              margin="normal"
+              inputRef={register({
+                required: "You must provide a age.",
+                pattern: {
+                  value: /^(?:1[01][0-9]|120|1[6-9]|[2-9][0-9])$/,
+                  message: "Your age must be between 16 and 120!"
+                }
+              })}
+              required
+              fullWidth
+              id="age"
+              label="Age"
+              name="age"
+              autoComplete="age"
+            />
+            {errors.age && <div>{errors.age.message}</div>}
 
           <TextField
             variant="outlined"
@@ -85,7 +130,7 @@ export default function SignIn() {
             label="Email Address"
             name="email"
             autoComplete="email"
-            autoFocus
+            // autoFocus
           />
           {errors.email && <div>{errors.email.message}</div>}
           <TextField
@@ -106,6 +151,26 @@ export default function SignIn() {
             id="password"
             autoComplete="current-password"
           />
+
+          <TextField
+            variant="outlined"
+            margin="normal"
+            inputRef={register({
+              required: 'You must provide an animal.',
+              pattern: {
+                value: /^([^0-9]*)$/,
+                message: "Please provide only letters!"
+              },
+             
+            })}
+            required
+            fullWidth
+            name="animal"
+            label="Animal"
+            autoComplete="loved-animal"
+          />
+          {errors.animal && <div>{errors.animal.message}</div>}
+
           {errors.password && <div>{errors.password.message}</div>}
           <FormControlLabel
             control={
@@ -142,8 +207,9 @@ export default function SignIn() {
         </form>
       </div>
       <Box mt={8}>
-        <Copyright />
       </Box>
     </Container>
+    <h1>{message}</h1>
+    </div>
   );
 }
